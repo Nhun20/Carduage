31e8b14c29de663819608404e882e7fc6a313d24
Nhun <112807043+Nhun20@users.noreply.github.com>
2024-08-26 20:57:51 +0200

    Fixed printing

--- carduage.py
+++ carduage.py
@@ -2,33 +2,37 @@ import random
 
 class Deck:
     def __init__(self):
-        suits = ['S', 'H', 'D', 'C']  
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        self.cards = [f"{rank}{suit}" for suit in suits for rank in ranks]
-        self.cards += ["J1", "J2", "J3"]  
-        random.shuffle(self.cards)  
+        self.cards = self.generate_standard_deck()
 
-    def move_top_to_bottom(self, n=1):
-        if n > 0 and n <= len(self.cards):
-            self.cards = self.cards[n:] + self.cards[:n]
+    def generate_standard_deck(self):
+        suits = ['H', 'D', 'C', 'S']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        return [value + suit for suit in suits for value in values]
+
+    def get_card(self, index):
+        if 0 <= index < len(self.cards):
+            return self.cards[index]
+        else:
+            raise IndexError(f"Index {index} out of range for deck of size {len(self.cards)}.")
+
+    def move_top_to_bottom(self, n):
+        self.cards = self.cards[n:] + self.cards[:n]
 
     def flip_deck(self):
         self.cards.reverse()
 
     def cut_deck(self, n):
-        if n >= 0 and n <= len(self.cards):
-            new_deck = self.cards[:n]
-            self.cards = self.cards[n:]
-            return new_deck
+        top_half = self.cards[:n]
+        self.cards = self.cards[n:]
+        return top_half
 
-    def peek_top(self, n=1):
-        return self.cards[:n] if n > 0 else []
+    def peek_top(self, n):
+        return self.cards[:n]
 
-    def take_top(self, n=1):
-        if n > 0 and n <= len(self.cards):
-            top_cards = self.cards[:n]
-            self.cards = self.cards[n:]
-            return top_cards
+    def take_top(self, n):
+        taken = self.cards[:n]
+        self.cards = self.cards[n:]
+        return taken
 
     def count_cards(self):
         return len(self.cards)
@@ -37,8 +41,7 @@ class Deck:
         random.shuffle(self.cards)
 
     def insert_deck(self, new_deck, pos):
-        if 0 <= pos <= len(self.cards):
-            self.cards = self.cards[:pos] + new_deck + self.cards[pos:]
+        self.cards = self.cards[:pos] + new_deck + self.cards[pos:]
 
     def __str__(self):
-        return ", ".join(self.cards)
+        return ', '.join(self.cards)
